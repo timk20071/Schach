ae373aa71897b456acc4ce58bbc0cc2c5d0f6cfa
lekrotte22 <jonas.hayderer@gmail.com>
2024-04-18 10:07:33 +0200

    Added Spielbrett

---
+++ 029_Schach/Spielbrett.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _029_Schach
+{
+    internal class Spielbrett
+    {
+        Figur[][] Brett = new Figur[8][8];
+
+        public void Print(Figur[][] Brett)
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                for (int j = 0; i <= 8; j++)
+                {
+                    Console.WriteLine(Brett[i][j]);
+                }
+            }    
+        }
+    }
+}
