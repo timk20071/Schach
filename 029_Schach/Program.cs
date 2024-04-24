de4e0be6fdf4770163dd5353c6c972ccfb722363
lekrotte22 <jonas.hayderer@gmail.com>
2024-04-24 08:35:50 +0200

    Ausgabe für Spielbrett

--- 029_Schach/Program.cs
+++ 029_Schach/Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace _029_Schach {
     internal class Program {
         static void Main(string[] args) {
-            //Hallo
 
-            Brett.Print();
+            Spielbrett Brett = new Spielbrett();
+            Figur Spieler = new Figur(true, 'A');
+            Figur[,] Bretter = new Figur[8, 8];
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Bretter[i, j] = Spieler;
+                }
+            }
+
+            Brett.Print(Bretter);
+            Console.ReadKey();
         }
     }
 }
